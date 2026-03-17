33d5ecb964fcdaa77c652d3f6832154ed1e69eb4
Janice Kim <203327415+jjuyeonkim@users.noreply.github.com>
2026-03-17 12:27:39 -0400

    Changing data to integer

--- full/coupler_main.F90
+++ full/coupler_main.F90
@@ -388,7 +388,7 @@ program coupler_main
   !$ser mode write
   !$ser on
   !$ser savepoint CouplerMain-Sanity
-  !$ser data full_coupler=Time_step_atmos
+  !$ser data full_coupler_current_timestep=current_timestep
   !$ser off
 
   call fms_init
